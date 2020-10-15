0871d19f948c8d2c24bb21610e10774dc819a243
youngjin-lee-otsk <youngjin.lee@one-line.com>
2020-10-15 14:45:48 +0900

    tmp

---
+++ main_2.py
@@ -0,0 +1,17 @@
+import requests
+
+def main():
+    # webhook url
+    url = "https://hooks.slack.com/services/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"
+
+    text = "꺼져"
+
+    payload = {
+        "text": text
+    }
+
+    requests.post(url, json=payload)
+
+# 이 스크립트에서 실행할 함수는 main
+if __name__ == "__main__":
+    main()
