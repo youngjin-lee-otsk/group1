fea92f4c7d7f3edb2e53eb17e8131fa4529f024a
simon2766 <simon2766@naver.com>
2020-10-19 14:25:32 +0900

    Adding Source of Handling data

--- read_gspread.py
+++ read_gspread.py
@@ -8,7 +8,7 @@ scope = [
 ]
 
 credentials = Credentials.from_service_account_file(
-    'C:/Python/group1/butterfly-257608-926179e2b552.json',
+    'C:/Users/2018136/Downloads/Python_S2/group1/butterfly-257608-926179e2b552.json',
     scopes=scope
 )
 
@@ -18,3 +18,29 @@ gc1 = gc.open("KYS_ButterflyProject").worksheet('Sample')
 gc2 = gc1.get_all_values()
 print(gc2)
 print(gc2[0])
+print('--------')
+"""
+[0] = ID
+[1] = Command
+[2] = Contents
+[3] = Contents
+""" 
+"""
+입력 : EMG_EDI
+출력 : 'KT:123', 'YJ:456', 'SM:789'
+"""
+inputdata = input()
+for idx, asd in enumerate(gc2):
+    #print(asd)
+    if asd[1] == inputdata:
+        for idx2, zxc in enumerate(gc2[idx]):
+            if idx2 > 1 :
+                print (zxc)
+                if zxc == "" :
+                    break
+        print("OK")
+    #print(asd[0])
+    #print(asd[1])
+    #print(asd[2])
+    #print(asd[3])
+    #print(asd[4])
